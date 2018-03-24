d9ddd836cd6566986b7115d83a0ffe7ee1615526
Małgorzata Sokół <malgorzata@e-sokol.pl>
2018-03-24 20:28:06 +0100

    Poprawa zapisu i odczytu z pliku

--- SpaceFleet/SpaceFleet/src/tests/test4.cpp
+++ SpaceFleet/SpaceFleet/src/tests/test4.cpp
@@ -16,9 +16,11 @@ int test4() {
 	
 	Weapon newWeapon;
 	Weapon nextWeapon;
+	Weapon AI("AI", 100, -670, -200, 10000);
 	
 	myArsenal.add(newWeapon);
 	myArsenal.add(nextWeapon);
+	myArsenal.add(AI);
 	
 	myArsenal.display();
 	return 0;
