390ce85a48895626a86f9680d3a2a06dbbcb9238
Małgorzata Sokół <malgorzata@e-sokol.pl>
2018-03-30 22:40:57 +0200

    Usuniecie nadmiarowego testu

--- SpaceFleet/SpaceFleet/src/tests/test6.cpp
+++ SpaceFleet/SpaceFleet/src/tests/test6.cpp
@@ -1,4 +1,4 @@
-//Testowanie funkcji klasy Unit
+//Testowanie Menu
 
 #include <iostream>
 #include <string>
@@ -7,55 +7,25 @@
 #include "Combat.hpp"
 #include "Arsenal.hpp"
 #include "Unit.hpp"
+#include "Menu.hpp"
 
 using namespace std;
 
 string shipFlex(int num);
 
-int test5() {
-	int d = 0;
-	
-// [ship type](name, velocity, scope, durability, combatValue/capacity/-)
-	
-	Ship* ptr1 = new Ship("XR027");
-	Cargo* ptrCargo1 = new Cargo("CHROM");
-	Cargo* ptrCargo2 = new Cargo("MIX");
-	Cargo* ptrCargo3 = new Cargo("COS012");
-	Combat* ptrCombat1 = new Combat("TAS001");
-	Combat* ptrCombat2 = new Combat("Attic001");
-	Combat* ptrCombat3 = new Combat("Attic002");
-	
-	Unit myUnit("UnitTest001");
-	
-	myUnit.add(ptr1);
-	myUnit.add(ptrCargo1);
-	myUnit.add(ptrCargo2);
-	myUnit.add(ptrCargo3);
-	myUnit.add(ptrCombat1);
-	myUnit.add(ptrCombat2);
-	myUnit.add(ptrCombat3);
-	
-	myUnit.display();
-	myUnit.displayMembers();
-	
-	myUnit.setVelocity();
-	if(myUnit.getVelocity() != 80)
-		d++;
-	myUnit.setScope();
-	if(myUnit.getScope() != 30)
-		d++;
-	myUnit.setDurability();
-	if(myUnit.getDurability() != (1000+7500+1000+500+700+20+60)/7)
-		d++;
-	myUnit.setCombatValue();
-	if(myUnit.getCombatValue() != (1400/7))
-		d++;
-	myUnit.setCapacity();
-	if(myUnit.getCapacity() != (1200+1000+4500) )
-		d++;
-	
-	cout << "Parameters have been updated" << endl;
-	myUnit.display();
-	
-	return d;
+int test6() {
+//	int i;
+//	Menu menu(5, 30, 10);
+//	menu.display();
+//	cin >> i;
+//	while(!(cin.good()) || i<1 || i>3) {
+//		cin.clear();
+//		cin.ignore(1000, '\n');
+//		cin >> i;
+//	}
+//	menu.extend(i);
+
+	Menu menu(5, 30, 25);
+	menu.hello();
+	return 0;
 }
